aeadcdf21a0b2d72301ea2bba957302c5b85e949
Tony1396 <36861259+Tony1396@users.noreply.github.com>
2018-03-18 21:34:50 -0600

    Update Servicio_Crear_Usuario.java
    Se agregaron comentarios

--- src/negocio/Servicio_Crear_Usuario.java
+++ src/negocio/Servicio_Crear_Usuario.java
@@ -1,18 +1,42 @@
 package negocio;
+/**
+ * @author anthony
+ * La clase Servicio_Crear_Usuario se encarga de pasar los datos al DAO_Usuario
+ * y que este los maneje, este servicio maneja los objetos de tipo Usuario para
+ * pasar los datos
+ * 
+ */
 
 import java.sql.SQLException;
+
+import javax.swing.JOptionPane;
 
 import datos.DAO_Usuario;
 
 public class Servicio_Crear_Usuario {
 	DAO_Usuario daou;
 
+	/**
+	 * Constructor del Servicio
+	 * @param daou2
+	 */
 	public Servicio_Crear_Usuario(DAO_Usuario daou2) {
 		daou = daou2;
 	}
-
+	
+	
+	
+	/**
+	 * ESte metodo revisa si el usuario ya esxiste e la base de datos,
+	 * Dependiendo el resultado devuelve una respuesta en forma de booleano 
+	 * 
+	 * @param nombre2
+	 * @param nick2
+	 * @param contrasenia2
+	 * @return
+	 * @throws SQLException
+	 */
 public boolean valida(String nombre2, String nick2, String contrasenia2) throws SQLException {
-
 		
 		Usuario[] usu = daou.Retrieve();
 		String nom;
@@ -23,10 +47,8 @@ public boolean valida(String nombre2, String nick2, String contrasenia2) throws 
 			
 			nom = usu[i].getNombre();
 			nick = usu[i].getNick();
-
 			cont =usu[i].getContrasenia();
 			if (nom.contains(nombre2) && nick.contains(nick2) && cont.contains(contrasenia2)) {
-
 				return false;
 			}
 		}
@@ -34,18 +56,31 @@ public boolean valida(String nombre2, String nick2, String contrasenia2) throws 
 		return true;
 	}
 
+/**
+ * Metodo booleano para insertar usuarios en la base de datos
+ * , prImero valida si ya existe y  si no es asi, entonces si les pasa los datos al DAO
+ * 
+ * @param nombre
+ * @param apellido
+ * @param asesor
+ * @param contraseña
+ * @param nick
+ * @return
+ * @throws SQLException
+ */
 
-	public boolean agregarUsuario(String nombre, String apellido, int asesor, String contrasenia, String nick) throws SQLException {
-		if(valida(nombre,nick,contrasenia) == true) {
-				Usuario u= new Usuario(nombre, apellido, asesor,contrasenia, nick);
-
-				if(daou.crear(u) == true) {
+	public boolean agregarUsuario(String nombre, String apellido, int asesor, String contraseña, String nick) throws SQLException {
+		if (valida(nombre, nick, contraseña) == true) {
+			Usuario u = new Usuario(nombre, apellido, asesor, contraseña, nick);
+			if (daou.crear(u) == true) {
 				return true;
-			}else {
+			} else {
 				return false;
 			}
-		}
-		return false;	
+		}else {
+			JOptionPane.showMessageDialog(null, "El usuario ya existe");
+			return true;
+		}	
 	}
 
 }
